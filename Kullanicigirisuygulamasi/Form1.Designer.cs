faa873a27029e42f4446705487513d7680d4eff9
Fausa23 <erdinsavas1@gmail.com>
2023-10-04 09:08:20 +0300

    Add project files.

---
+++ Kullanicigirisuygulamasi/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Kullanicigirisuygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_kadi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_sifre = new System.Windows.Forms.TextBox();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // tb_kadi
+            // 
+            this.tb_kadi.BackColor = System.Drawing.SystemColors.Desktop;
+            this.tb_kadi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tb_kadi.Location = new System.Drawing.Point(82, 33);
+            this.tb_kadi.Name = "tb_kadi";
+            this.tb_kadi.Size = new System.Drawing.Size(183, 20);
+            this.tb_kadi.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(15, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre\r\n";
+            // 
+            // tb_sifre
+            // 
+            this.tb_sifre.BackColor = System.Drawing.SystemColors.Desktop;
+            this.tb_sifre.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tb_sifre.Location = new System.Drawing.Point(82, 69);
+            this.tb_sifre.Name = "tb_sifre";
+            this.tb_sifre.Size = new System.Drawing.Size(183, 20);
+            this.tb_sifre.TabIndex = 3;
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btn_giris.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_giris.Location = new System.Drawing.Point(190, 95);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(75, 23);
+            this.btn_giris.TabIndex = 4;
+            this.btn_giris.Text = "GİRİŞ YAP";
+            this.btn_giris.UseVisualStyleBackColor = false;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(290, 185);
+            this.Controls.Add(this.btn_giris);
+            this.Controls.Add(this.tb_sifre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_kadi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kullanıcı Giriş";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_kadi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_sifre;
+        private System.Windows.Forms.Button btn_giris;
+    }
+}
+
---
+++ Kullanicigirisuygulamasi/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kullanicigirisuygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_giris_Click(object sender, EventArgs e)
+        {
+            string kullanici = tb_kadi.Text;
+            string sifre = tb_sifre.Text;
+            if (kullanici == "admin" && sifre == "12345")
+            {
+                MessageBox.Show("Hoşgeldin Admin", "Giriş Başarılı");
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adını veya şifreyi kontol ediniz", "Giriş Başarısız");
+            }
+        }
+    }
+}
